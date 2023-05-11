eae3fd25e6f6cb5a561121a82045a7f50a53f676
pailime <emilia.palczyk@gmail.com>
2023-05-11 23:35:03 +0200

    urls

--- start_helping/start_helping/urls.py
+++ start_helping/start_helping/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from helping_app.views import LandingPageView, AddDonationView, LoginView, RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='landing_page'),
+    path('add_donation/', AddDonationView.as_view(), name='add_donation'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
