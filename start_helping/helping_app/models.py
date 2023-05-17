226272596a19c8e0c4856694e9098a48bd44bd4e
pailime <emilia.palczyk@gmail.com>
2023-05-17 23:47:51 +0200

    create AbstractUser

--- start_helping/helping_app/models.py
+++ start_helping/helping_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, password):
+#         if not email:
+#             raise ValueError('Users must have an email address')
+#
+#         user = self.model(email=self.normalize_email(email))
+#
+#         user.set_password(password)
+#         user.save()
+#         return user
+#
+#     def create_superuser(self, email, password):
+#         user = self.create_user(email, password=password)
+#         user.is_admin = True
+#         user.is_superuser = True
+#         user.is_staff = True
+#         user.save()
+#         return user
+#
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    # objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
